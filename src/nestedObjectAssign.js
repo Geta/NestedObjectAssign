1c2276bdb3a3677379146827be0519ad4e3102e1
Eirik Horvath <eirik@geta.no>
2017-12-21 15:57:50 +0100

    Fixed bug where array is not concatted, but rather final value is king.

--- src/nestedObjectAssign.js
+++ src/nestedObjectAssign.js
@@ -1,4 +1,5 @@
-import {isObject} from './isObject';
+import { isObject } from './isObject';
+import { isArray } from './isArray';
 
 export default function nestedObjectAssign(target, ...sources){
     if (!sources.length)
@@ -9,12 +10,18 @@ export default function nestedObjectAssign(target, ...sources){
     if (isObject(target) && isObject(source)){
         for (const key in source){
             if (isObject(source[key])){
-                if (!target[key])
+                if (!target[key]) {
                     Object.assign(target, {[key]: {}});
+                }
 
                 nestedObjectAssign(target[key], source[key]);
-            }
-            else {
+            } else if (isArray(source[key])) {
+                if (!target[key]) {
+                    Object.assign(target, {[key]: []});
+                }
+
+                target[key] = target[key].concat(source[key]);
+            } else {
                 Object.assign(target, {[key]: source[key]});
             }
         }
