3e144cf093f0f5302d610c301bf320ed9b4d7992
Eirik Horvath <eirik@geta.no>
2017-12-21 15:58:15 +0100

    Added array to the mock data in test to validate that arrays now concat correctly.

--- test/nestedObjectAssign.spec.js
+++ test/nestedObjectAssign.spec.js
@@ -1,29 +1,45 @@
 import { expect } from 'chai';
 import nestedObjectAssign from '../index.js';
 
-var mockData = {
+const mockData = {
     default: {
         heading: 'title',
         body: {
             paragraph: 'p',
             heading: 'h1'
-        }
+        },
+        products: [
+            {
+                paragraph: 'p',
+                heading: 'h1'
+            }
+        ]
     },
     first: {
         body: {
             span: 'span',
             header: 'header'
-        }
+        },
+        products: [
+            {
+                span: 'span',
+                header: 'header'
+            }
+        ]
     },
     second: {
         body: {
             heading2: 'h2'
-        }
+        },
+        products: [
+            {
+                heading2: 'h2'
+            }
+        ]
     }
-
 };
 
-var expectedData = {
+const expectedData = {
     heading: 'title',
     body: {
         paragraph: 'p',
@@ -31,11 +47,24 @@ var expectedData = {
         span: 'span',
         header: 'header',
         heading2: 'h2'
-    }
+    },
+    products: [
+        {
+            paragraph: 'p',
+            heading: 'h1'
+        },
+        {
+            span: 'span',
+            header: 'header'
+        },
+        {
+            heading2: 'h2'
+        }
+    ]
 };
 
 describe('Given an instance of nestedObjectAssign', function() {
-    describe('when i merge the mockData', function() {
+    describe('when I merge the mockData', function() {
         it('it should be equal to expectedData', () => {
             expect(JSON.stringify(nestedObjectAssign({}, mockData.default, mockData.first, mockData.second))).to.be.equal(JSON.stringify(expectedData));
         });
